8aeb8877d6103bd834936991d084bdd053a45917
Marc Hughes <marc.hughes@gmail.com>
2013-12-06 11:02:20 -0500

    Publishing content

--- publish.sh
+++ publish.sh
@@ -3,6 +3,8 @@ echo "Adding files"
 git add .
 echo "Comming files"
 git commit -a -m "Publishing content"
+echo "Pushing"
+git push
 cd ../scrumdo-static
 echo "Pulling down changes into prod"
 git pull dev gh-pages
